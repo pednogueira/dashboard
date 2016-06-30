26208e87c465e00f6e93318b2b3cc4e77504fe15
Pedro Nogueira <pedro.silvestre@uva.br>
2016-06-30 20:02:23 -0300

    40pronto

--- index.php
+++ index.php
@@ -3,11 +3,13 @@
 <head>
 	<title>Dashboard</title>
 
-	<!-- css -->
-	
-	
+	<!-- css -->	
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" type="text/css" href="css/grids.css">
+
+	<!-- javascript -->
+	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+	<script type="text/javascript" src="_js/charts.js"></script>
 
 </head>
 <body>
@@ -26,34 +28,113 @@
 			</div>
 			<div class="col2">
 				<div class="box2">
-					<p>Receita aprovada</p>
-					<span>R$209.412,60</span>
+					<p>Pedidos aprovados</p>
+					<span>915</span>
+				</div>
+			</div>
+			<div class="col2">
+				<div class="box2">
+					<p>Ticket médio aprovado</p>
+					<span>R$228,87</span>
 				</div>
 			</div>
 			<div class="col2">
 				<div class="box2">
 					<p>Receita aprovada</p>
-					<span>R$209.412,60</span>
+					<span>R$228,87</span>
 				</div>
 			</div>
 			<div class="col2">
 				<div class="box2">
+					<div class="error">
+						<p class="middle">Produtos com erro de sincronização</p>
+						<span class="middle">120</span>
+					</div>
+				</div>
+			</div>
+			<div class="col2">
+				<div class="box2 ">
 					<p>Receita aprovada</p>
 					<span>R$209.412,60</span>
 				</div>
 			</div>
-			<div class="col2">
+		</section>
+		<section>
+			<article class="col8">
 				<div class="box2">
-					<p>Receita aprovada</p>
-					<span>R$209</span>
+					<h2>Histórico por preço <span>
+						<a href=""><img src="img/btnbox.png"></a></span>
+					</h2>
+					<div class="grafico"  id="grafico1">
+					</div>
 				</div>
-			</div>
-			<div class="col2">
 				<div class="box2">
-					<p>Receita aprovada</p>
-					<span>R$209.412,60</span>
+					<h2>Histórico por preço <span>
+						<a href=""><img src="img/btnbox.png"></a></span>
+					</h2>
+					<div class="grafico">
+						<p>teste</p>
+					</div>
 				</div>
-			</div>
+			</article>
+			<article class="col4 right">
+				<div class="box2">
+					<h2>Vendas por região<span>
+						<a href=""><img src="img/btnbox.png"></a></span>
+					</h2>
+					<div class="box-table">
+						<table class="table">
+							<thead>
+								<tr>
+									<th>Estado</th>
+									<th>Cidade</th>
+									<th>Venda</th>
+									<th>Valor total</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>Rio de Janeiro</td>
+									<td>Petrópolis</td>
+									<td>12</td>
+									<td>R$2.000,00</td>
+								</tr>
+								<tr>
+									<td>Rio de Janeiro</td>
+									<td>Petrópolis</td>
+									<td>12</td>
+									<td>R$2.000,00</td>
+								</tr>
+								<tr>
+									<td>Rio de Janeiro</td>
+									<td>Petrópolis</td>
+									<td>12</td>
+									<td>R$2.000,00</td>
+								</tr>
+								<tr>
+									<td>Rio de Janeiro</td>
+									<td>Petrópolis</td>
+									<td>12</td>
+									<td>R$2.000,00</td>
+								</tr>
+								<tr>
+									<td>Rio de Janeiro</td>
+									<td>Petrópolis</td>
+									<td>12</td>
+									<td>R$2.000,00</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</article>
+			<article class="col4 right">
+				<div class="box2">
+					<h2>Pedidos<span>
+						<a href=""><img src="img/btnbox.png"></a></span>
+					</h2>
+				</div>
+			</article>
 		</section>
 	</div>
 </body>
